06927efcfe559ad93cd1f6e0f7c62f6586a270fa
Reid Sanders <res229@lehigh.edu>
2026-09-13 19:38:08 -0400

    Guess evaluation logic

--- Wordle/wordle.c
+++ Wordle/wordle.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 
 char** loadDictionary(const char *filename, int *wordCount){
 	FILE *file = fopen(filename, "r");
@@ -39,3 +41,39 @@ char** loadDictionary(const char *filename, int *wordCount){
 }
 
 
+void evaluateGuess(const char *target, const char *guess){
+	int state[5] = {0};
+	int targetUsed[5] = {0};
+	int guessUsed[5] = {0};
+
+	for(int i = 0; i < 5; i++){
+		if(guess[i] == target[i]){
+			state[i] = 2;
+			targetUsed[i] = 1;
+			guessUsed[i] = 1;
+		}
+	}
+
+	for(int i = 0; i < 5; i++){
+		if(guessUsed[i])) continue;
+		for(int j = 0; j < 5; j++){
+			if(!targetUsed[j] && guess[i] == target[j]){
+				state[i] = 1;
+				targetUsed[j] = 1;
+				break;
+			}
+		}
+	}
+
+
+	for(int i = 0; i < 5; i++){
+		if(state[i] == 2){
+			printf("\033[1;32m[%c]\033[0m", toupper(guess[i]));
+		}else if (state[i] == 1){
+			printf("\033[1;30m[%c]\033[0m", toupper(guess[i]));
+		}else{
+			printf("\033[1;30m[%c]\033[0m", toupper(guess[i]));
+		}
+	}
+	printf("\n");
+}
