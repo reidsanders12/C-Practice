d7ed5490ddd06e78bbe71d149185b002df553470
Reid Sanders <res229@lehigh.edu>
2026-09-13 12:22:51 -0400

    MiniNotepad skeleton model

---
+++ MiniNotepad.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char** addLine(char **lines, int *lineCount, int *capacity){
+//Captures user input, handles array resizing if full, and allocates memory for the new string
+}
+
+void printNotepad(char **lines, int lineCount){
+//Iterates through the line pointers and displays the current buffer with line numbers
+}
+
+void freeNotepad(char **lines, int lineCount){
+//Frees every individual line string first, then frees the main lines array pointer to prevent leaks
+}
+
+int main(void){
+//Implement functions
+}
