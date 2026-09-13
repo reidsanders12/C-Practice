e5f4dda5c3c9728201bd4e8a767ec02f539eb0e3
Reid Sanders <res229@lehigh.edu>
2026-09-13 12:40:59 -0400

    addLine function done

--- MiniNotepad.c
+++ MiniNotepad.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char** addLine(char **lines, int *lineCount, int *capacity){
 //Captures user input, handles array resizing if full, and allocates memory for the new string
+	if (*lineCount >= *capacity){
+		*capacity = (*capacity == 0) ? 4 : *capacity * 2;
+		char **temp = realloc(lines, (*capacity) * sizeof(char*)); 
+		if (temp == NULL){
+			fprintf(stderr, "Error: Failed to allocate memory.\n");
+			exit(EXIT_FAILURE);
+		}
+		lines = temp;
+	}
+	char buffer[256];
+	printf("Enter line %d: ", *lineCount + 1);
+	if (fgets(buffer, sizeof(buffer), stdin) != NULL){
+		buffer[strcspn(buffer, "\n")] = '\0';
+		lines[*lineCount] = malloc(strlen(buffer) + 1);
+		if (lines[*lineCount] == NULL){
+			fprintf(stderr, "Error: Failed to allocate memory.\n");
+			exit(EXIT_FAILURE);
+		}
+	strcpy(lines[*lineCount], buffer);
+	(*lineCount)++;
+	}
+	return lines;
 }
+
 
 void printNotepad(char **lines, int lineCount){
 //Iterates through the line pointers and displays the current buffer with line numbers
