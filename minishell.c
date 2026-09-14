d80bf31ea70cb0d7443b5562168dc2af84869259
Reid Sanders <res229@lehigh.edu>
2026-09-14 10:15:00 -0400

    minishell skeleton model

---
+++ minishell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define MAX_LINE 1024
+#define MAX_ARGS 64
+
+void parse_input(char *line, char **args){
+	//splits raw input line into tokens for execution
+}
+
+int main(){
+
+}
