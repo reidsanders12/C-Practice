4eb40f8cb7f300358da4bff8ce4a193b230c7965
Reid Sanders <res229@lehigh.edu>
2026-09-14 10:17:36 -0400

    parse_input finished

--- minishell.c
+++ minishell.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 #define MAX_LINE 1024
 #define MAX_ARGS 64
 
 void parse_input(char *line, char **args){
 	//splits raw input line into tokens for execution
+	int i = 0;
+	args[i] = strtok(line, " \t\n");
+	while(args[i] != NULL && i < MAX_ARGS - 1){
+		i++;
+		args[i] = strtok(NULL, " \t\n");
+	}
+	args[i] = NULL;
 }
 
 int main(){
